4a1d458aeab17bd5cd289801be3262fbbad5eb73
riddhi2707 <76160932+riddhi2707@users.noreply.github.com>
2023-09-19 22:07:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+const divtoShow='nav .menu';
+const divPopup= document.querySelector(divtoShow);
+const divTrigger= document.querySelector('.m-trigger');
+
+divTrigger .addEventListener('click', () => {
+    setTimeout(() => {
+        if(!divPopup.classList.contains('show')){
+            divPopup.classList.add('show');
+            document.body.classList.add('menu-visible')
+        }
+        
+    }, 250);
+})
+
+//automatically close by click outside menu
+
+document.addEventListener('click', (e) => {
+    const isClosest = e.target.closest(divtoShow);
+
+    if(!isClosest && divPopup.classList.contains('show')){
+        divPopup.classList.remove('show');
+        document.body.classList.remove('menu-visible')
+    }
+})
+
+//............SEARCH
+
+const sTrigger= document.querySelector('.s-trigger');
+const addclass= document.querySelector('.site');
+sTrigger.addEventListener('click', () => {
+    addclass.classList.toggle('showsearch')
+})
+
+//............Slider
+
+const sliderThumb = new Swiper('.thumb-nav', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    slidesPerGroup: false,
+    breakpoints: {
+        992: {
+            direction: 'vertical'
+        }
+    }
+});
+const theSlider = new Swiper('.thumb-big', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+      },
+      thumbs: {
+        swiper: sliderThumb
+      }
+    
+});
+
+//.........Tabbed products
+
+const tabbednav = new Swiper('.tnav', {
+    spaceBetween: 20,
+    slidesPerView: 6,
+    centeredSlides: true,
+    slidesPerGroup: false,
+   
+});
+const theTab = new Swiper('.tabbed-item', {
+    loop: true,
+    slidesPerView: 1,
+    autoHeight: true,
+    thumbs: {
+        swiper: tabbednav,
+    }
+    
+});
+
+// ......... ON SCROLL TRANSITION
+
+const io = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > 0){
+            entry.target.classList.add('this')
+        }
+    })
+});
+
+
+const box= document.querySelectorAll('.animate');
+    box.forEach((el) => {
+        io.observe(el);
+    });
+
