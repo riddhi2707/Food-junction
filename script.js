6ddc109a11f57605853f55b8adf46ca02b37f242
riddhi2707 <76160932+riddhi2707@users.noreply.github.com>
2023-09-19 22:13:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,17 +75,17 @@ const theTab = new Swiper('.tabbed-item', {
 
 // ......... ON SCROLL TRANSITION
 
-const io = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.intersectionRatio > 0){
-            entry.target.classList.add('this')
-        }
-    })
-});
+// const io = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if(entry.intersectionRatio > 0){
+//             entry.target.classList.add('this')
+//         }
+//     })
+// });
 
 
-const box= document.querySelectorAll('.animate');
-    box.forEach((el) => {
-        io.observe(el);
-    });
+// const box= document.querySelectorAll('.animate');
+//     box.forEach((el) => {
+//         io.observe(el);
+//     });
 
